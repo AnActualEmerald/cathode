33054f5e75eb14296e98adebabf376faa197959d
AnActualEmerald <emerald_actual@proton.me>
2022-09-01 01:09:11 -0400

    frame preview component

---
+++ src/components/FramePreview.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let index: number;
+
+  //TODO: load frame from ray
+</script>
+
+<div class="preview" />
+
+<style lang="scss">
+  .preview {
+    border-radius: 10px;
+    background-color: rgba(150, 150, 150, 0.5);
+    width: 15vh;
+    height: 15vh;
+  }
+</style>
